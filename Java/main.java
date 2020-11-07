50b0ddde3d74e29a077cd134c4c87ea4ca104948
Julian Carvajal <juli19cam@gmail.com>
2020-11-07 12:08:45 -0500

    Little changes

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "PWD450";
+        car.driver = "Pepito Perez";
+        car.passegenger = 4;
+        car.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,9 @@
+from car import Car
+
+
 if __name__ == "__main__":
     print('Hola mundo')
+    car = Car()
+    car.license = "JEL681"
+    car.driver = "Julio Cesar"
+    print(vars(car))
