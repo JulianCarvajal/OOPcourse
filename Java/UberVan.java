97526c94b5eda024eef84f3b012e71d90c6db351
Julian Carvajal <juli19cam@gmail.com>
2020-11-09 22:30:03 -0500

    Polimorfismo con Java

--- Java/UberVan.java
+++ Java/UberVan.java
@@ -4,13 +4,19 @@ import java.util.Map;
 class UberVan extends Car {
     Map<String, Map<String, Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
+    protected Integer passenger;
 
-    public UberVan (String license, Account driver, Map<String, 
-    Map<String, Integer>> typeCarAccepted,
-    ArrayList<String> seatsMaterial){
+    public UberVan (String license, Account driver){
         super(license, driver);
-        this.typeCarAccepted = typeCarAccepted;
-        this.seatsMaterial = seatsMaterial;
+    }
+
+    @Override
+    public void setPassenger(Integer passenger) {
+        if(passenger == 6){
+            this.passenger = passenger;
+        }else{
+            System.out.println("Necesitas asignar 6 pasajeros");
+        }
     }
 }
 
--- Java/main.java
+++ Java/main.java
@@ -4,5 +4,15 @@ class Main{
         Car car = new Car("PWD450", new Account("Pepito Perez", "PEP531"));
         car.setPassenger(4);
         car.printDataCar();
+
+        System.out.println();
+        UberX uberX = new UberX("JEG353", new Account("Martin Salgado", "4135457"), "Renault", "Logan")        ;
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+
+        System.out.println();
+        UberVan uberVan = new UberVan("TER854", new Account("Jose Carmona", "54334182"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
     }
 }
